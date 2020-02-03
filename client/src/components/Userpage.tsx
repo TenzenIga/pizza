c1d6ca67a0cd30bf3bae3313c5e7ec6df8f28105
TenzenIga <arhan-93@list.ru>
2020-02-03 05:47:59 +0300

    last rey

--- client/src/components/Userpage.tsx
+++ client/src/components/Userpage.tsx
@@ -11,11 +11,11 @@ export default function Userpage() {
        .then(response => setOrders(response.data))
     }, [])
 
-    let ordersList = orders && orders.map((key:number,element:any) => <p key={key}>{element.body}</p>  )
+    
     return (
         <div>
             <h2>Orders history</h2>
-            {ordersList}
+            
         </div>
     )
 }
