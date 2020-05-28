7daad6738b6b6ac33cb7931c9d8e47a47a74e97a
TenzenIga <arhan-93@list.ru>
2020-05-28 23:42:35 +0300

    deleted js files

--- server/middleware/validateId.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(req, res, next) {
-    if (isNaN(parseInt(req.params.id)))
-        return res.status(404).send('Invalid ID');
-    next();
-}
-exports.default = default_1;
